6f8128dbc22b265c1e9cf4aa032273f84d7b6530
Amy Simmons <amy__simmons@hotmail.com>
2016-05-27 18:19:11 +1000

    code working with cleaner html :) but its not very pretty

--- hashtags.js
+++ hashtags.js
@@ -18,46 +18,46 @@ document.addEventListener("DOMContentLoaded", function(event) {
       for (var i = 0; i < words.length; i++) {
         var word = words[i];
         if(word[0] == "#"){
-          words[i] = wrapHashtagInLink(word);
-          console.log('hash tag: link', words[i]);
 
-          node.nodeValue = words.join(' ');
-          debugger
+          var beforeHashtag = words.slice(0, i);
+          var beforeTextNode = document.createTextNode(" " + beforeHashtag.join(' '));
+          var replacementNode = document.createElement('span');
+          var link;
+          var afterHashtag;
+          var afterTextNode;
 
+          //check for hashtag breaking chars
+          var index = word.indexOf(word.match(/[^A-Za-z0-9#+]/));
+
+          if(index > 0){
+            var hashtag = word.substring(0, index);
+            link = wrapHashtagInLink(hashtag);
+            var remainder = word.slice(index);
+            afterHashtag = words.slice(i + 1);
+            afterTextNode = document.createTextNode(remainder + " " + afterHashtag.join(' '));
+          }else {
+            var hashtag = word;
+            link = wrapHashtagInLink(hashtag);
+            afterHashtag = words.slice(i + 1);
+            afterTextNode = document.createTextNode(" " + afterHashtag.join(' '));
+          }
+
+          replacementNode.appendChild(beforeTextNode);
+          replacementNode.appendChild(link);
+          replacementNode.appendChild(afterTextNode);
+
+          node.parentNode.insertBefore(replacementNode, node);
+          node.parentNode.removeChild(node);
         }
       };
   }
 
-  function wrapHashtagInLink(word){
-
-    //check for hashtag breaking chars
-    var index = word.indexOf(word.match(/[^A-Za-z0-9#+]/));
-
-    if(index > 0){
-      var hashtag = word.substring(0, index);
-      var link = "<a href='https://twitter.com/search?q=" + hashtag + "'>" + hashtag + "</a>";
-      return word.replace(hashtag, link);
-    }else{
-      return "<a href='https://twitter.com/search?q=" + word + "'>" + word + "</a>";
-    }
-
-    // var link = document.createElement('a');
-
-    // if(index > 0){
-
-    //   var hashtag = word.substring(0, index);
-    //   var linkText = document.createTextNode(hashtag);
-    //   link.appendChild(linkText);
-    //   link.href = "https://twitter.com/search?q=" + hashtag;
-    //   return word.replace(hashtag, link);
-
-    // }else{
-    //   var linkText = document.createTextNode(word);
-    //   link.appendChild(linkText);
-    //   link.href = "https://twitter.com/search?q=" + word;
-    //   return link;
-    // }
-
+  function wrapHashtagInLink(hashtag){
+    var link = document.createElement('a');
+    var linkText = document.createTextNode(" " + hashtag);
+    link.appendChild(linkText);
+    link.href = "https://twitter.com/search?q=" + hashtag;
+    return link;
   }
 
   traverseDom(document.body);
